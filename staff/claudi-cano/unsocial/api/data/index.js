0a299746b6a6bfb7d624d440ad845c4bf1fe32d7
Claudi1991 <claudicano@hotmail.com>
2024-10-31 13:17:57 +0100

    update test folder with xhr #183

---
+++ staff/claudi-cano/unsocial/api/data/index.js
@@ -0,0 +1,9 @@
+import storage from './users.js'
+
+import uuid from './uuid.js'
+
+export {
+    storage,
+
+    uuid
+}
---
+++ staff/claudi-cano/unsocial/api/logic/authenticateUser.test.js
@@ -0,0 +1,7 @@
+import authenticateUser from "./authenticateUser";
+
+try {
+    console.log(authenticateUser('cocodrilo', '123123123'))
+} catch (error) {
+    console.error(error)
+}
